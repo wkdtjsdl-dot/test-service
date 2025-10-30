ed708e93918db4be5bc3a0d452387bebc048e9c0
Jangseon Won <jswon25@gcgenome.com>
2025-10-31 08:45:11 +0900

    chore: sales-service 폴더 구조 정리

---
+++ src/main/kotlin/com/idrsys/ailis/sales/infrastructure/config/BaseConfig.kt
@@ -0,0 +1,45 @@
+package com.idrsys.ailis.sales.infrastructure.config
+
+import com.idrsys.common.kor2dbc.config.EnableCommOnMstSlvDataSource
+import com.idrsys.common.kor2dbc.datasource.CustomDatabaseProperties
+import com.idrsys.common.kor2dbc.datasource.DatabaseProperties
+
+import com.idrsys.reactive.excel.EnableReactiveExcel
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+
+@Configuration
+@EnableCommOnMstSlvDataSource
+@EnableR2dbcRepositories(
+    basePackages = ["com.idrsys.ailis.sales.adapter.repository"],
+    entityOperationsRef = "r2dbcEntityTemplate"
+)
+@EnableReactiveExcel
+class BaseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.r2dbc")
+    fun databaseProperties(): DatabaseProperties = DatabaseProperties()
+
+    @Bean
+    @ConfigurationProperties(prefix = "database")
+    fun customDatabaseProperties(): CustomDatabaseProperties = CustomDatabaseProperties()
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        val encodingId = "bcrypt"
+        val encoders = mapOf(
+            encodingId to BCryptPasswordEncoder()
+        )
+
+        return DelegatingPasswordEncoder(encodingId, encoders)
+    }
+}
---
+++ src/main/kotlin/com/idrsys/ailis/sales/infrastructure/config/ResolverConfiguration.kt
@@ -0,0 +1,20 @@
+package com.idrsys.ailis.sales.infrastructure.config
+
+import com.idrsys.reactive.config.EnableCommOnWebfluxConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
+
+@EnableCommOnWebfluxConfiguration
+@Configuration
+@ConditionalOnWebApplication(type = Type.REACTIVE)
+class ResolverConfiguration() : WebFluxConfigurer {
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        configurer.addCustomResolver(
+            ReactivePageableHandlerMethodArgumentResolver()
+        )
+    }
+}
