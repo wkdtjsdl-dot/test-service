2369fe4587ac75e3f15984a58ddba3dd0e5173fd
Jangseon Won <jswon25@gcgenome.com>
2026-02-03 15:14:49 +0900

    feat) 엑셀 Validation 추가
    Signed-off-by: Jangseon Won <jswon25@gcgenome.com>

--- src/main/kotlin/com/idrsys/ailis/sales/application/usecase/testCodeMapping/TestCodeMappingUseCase.kt
+++ src/main/kotlin/com/idrsys/ailis/sales/application/usecase/testCodeMapping/TestCodeMappingUseCase.kt
@@ -2,8 +2,10 @@ package com.idrsys.ailis.sales.application.usecase.testCodeMapping
 
 import com.idrsys.ailis.sales.application.dto.request.testCodeMapping.TestCodeMappingCommand
 import com.idrsys.ailis.sales.application.dto.request.testCodeMapping.TestCodeMappingSearchParam
+import com.idrsys.ailis.sales.application.dto.request.testCodeMapping.ValidateTstMappingRequest
 import com.idrsys.ailis.sales.application.dto.response.TestCodeMappingExcelValidResponse
 import com.idrsys.ailis.sales.application.dto.response.TestCodeMappingResponse
+import com.idrsys.ailis.sales.application.dto.response.ValidateTstMappingResponse
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -30,4 +32,12 @@ interface TestCodeMappingUseCase {
      * @return 내부 검사코드 (없으면 null)
      */
     suspend fun getTstCdByCustCdAndCustTstCd(custCd: String, custTstCd: String): String?
+
+    /**
+     * 고객 검사코드 매핑 일괄 유효성 검사
+     *
+     * @param request 유효성 검사 요청 DTO
+     * @return 유효하지 않은 검사코드 목록
+     */
+    suspend fun validateCustTstMappings(request: ValidateTstMappingRequest): ValidateTstMappingResponse
 }
