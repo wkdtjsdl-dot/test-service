bbda8ed4fdd155e8d604a2b7ee64c7edec07899c
김석인 <kimsukin@idrsystems.co.kr>
2026-05-12 16:38:26 +0900

    directAcctCd 제거

--- src/main/kotlin/com/idrsys/ailis/sales/application/dto/response/inner/ReqServiceClosingResponse.kt
+++ src/main/kotlin/com/idrsys/ailis/sales/application/dto/response/inner/ReqServiceClosingResponse.kt
@@ -4,14 +4,12 @@ package com.idrsys.ailis.sales.application.dto.response.inner
  * TstItem closing response from req-service (Inner API)
  */
 data class ReqServiceClosingResponse(
-    val updatedCount: Int,
-    val directAcctCd: String
+    val updatedCount: Int
 )
 
 /**
  * TstItem closing release response from req-service (Inner API)
  */
 data class ReqServiceClosingReleaseResponse(
-    val releasedCount: Int,
-    val directAcctCd: String
+    val releasedCount: Int
 )
